085e21dc99b8d53ed04eca5fd77083cefa8e0216
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-11-05 16:59:02 +0000

    added math equation 23 + 8 / 2

---
+++ program1/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Mr Alex
+ * @version 1.0.0
+ * @date 2025-11-5
+ * @fileoverview This program will display the answer and the following question 
+ */
+
+//This program will display the answer givin 
+console.log("A = ", 23 + 8)
+
+//This program will take 31 and divide it by 2
+console.log("A = ",31 / 2)
+
+console.log("\nDone")
