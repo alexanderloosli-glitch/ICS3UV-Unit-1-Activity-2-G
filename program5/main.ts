0135140618546f2e98deab1b3ddbaa3079083e0b
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-11-05 17:13:48 +0000

    added math equation 6 x 2 + 8

---
+++ program5/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Mr Alex
+ * @version 1.0.0
+ * @date 2025-11-5
+ * @fileoverview This program will display the answer and the following question
+ */
+
+//This program will display the answer givin 
+console.log("A = ", (6) * (2 + 8))
+
+console.log("\nDone")
