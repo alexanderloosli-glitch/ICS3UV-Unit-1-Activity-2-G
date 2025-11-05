1a4c29479811925deba2f0e6dff5ef67a498d195
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-11-05 17:03:49 +0000

    added math equation 3 + 8 * 2

---
+++ program2/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Mr Alex
+ * @version 1.0.0
+ * @date 2025-11-5
+ * @fileoverview This program will display the answer and the following question
+ */
+
+//This program will display the answer givin 
+console.log("A = ", 3 + 8)
+
+//This program will take 11 * 2 and display it 
+console.log("A = ", 11 * 2)
+
+console.log("\nDone")
